d210cfde70d691768a771a9db65fa689c30c338c
Tamer (tetocollctionway) <tamer@tetocollctionway.com>
2026-05-22 16:00:32 +0300

    🔧 Fix: Cleaned up imports and added services

--- lib/features/card3_document/document_screen.dart
+++ lib/features/card3_document/document_screen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+   import 'package:flutter/services.dart';
+   import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
